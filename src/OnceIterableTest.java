c34d3c803c09fcc9a0a0b5ea5e23b2df240c1def
leikos <Yvan@Yvans-MacBook-Air.local>
2013-09-15 21:12:11 -0700

    Adding OnceIterableTest

---
+++ src/OnceIterableTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.junit.Test;
+
+public class OnceIterableTest {
+
+	@Test
+	public void test() {
+		class OnceIterableArrayList extends ArrayList<String>
+		{
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = -5961789443447623011L;
+			private Iterator<String> fIterator = null;
+
+			@Override
+			public Iterator<String> iterator() {
+				if (fIterator == null)
+					fIterator = super.iterator();
+				return fIterator;
+			}			
+		}
+		OnceIterableArrayList list = new OnceIterableArrayList();
+		list.add("Hi");
+		list.add("Bye");
+		for (int i = 0; i < 2; i++)
+		{
+			// The second time nothing will be output
+			// because the iterator has been exhausted
+			for (String word : list)
+			{
+				System.err.println(word);
+			}
+		}
+		fail("Not yet implemented");
+	}
+
+}
